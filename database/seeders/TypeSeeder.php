085592a97885dff02ef17570db541c10e83be299
giuseppevignanello <peppe.vignanello@gmail.com>
2023-12-07 17:21:43 +0100

    seed some procedure type

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,16 +2,23 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class ProcedureTypeSeeder extends Seeder
+class TypeSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+        $types = ['Bug', 'Feature', 'Documentation'];
+
+        foreach ($types as $type) {
+            $newType = new Type();
+            $newType->name = $type;
+            $newType->save();
+        }
     }
 }
