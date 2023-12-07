9aeca7497eb94dbee5089dd466f91b6c6aecdb10
giuseppevignanello <peppe.vignanello@gmail.com>
2023-12-07 14:12:35 +0100

    add db connection and procedure model

---
+++ app/Http/Controllers/ProcedureController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Procedure;
+use App\Http\Requests\StoreProcedureRequest;
+use App\Http\Requests\UpdateProcedureRequest;
+
+class ProcedureController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProcedureRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Procedure $procedure)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Procedure $procedure)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProcedureRequest $request, Procedure $procedure)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Procedure $procedure)
+    {
+        //
+    }
+}
