ec94b8098f4715c8156dda6f1e17af00f725f475
giuseppevignanello <peppe.vignanello@gmail.com>
2023-12-07 14:14:03 +0100

    add procedureType and Employe model

---
+++ app/Http/Controllers/ProcedureTypeController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProcedureType;
+use App\Http\Requests\StoreProcedureTypeRequest;
+use App\Http\Requests\UpdateProcedureTypeRequest;
+
+class ProcedureTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProcedureTypeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProcedureType $procedureType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProcedureType $procedureType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProcedureTypeRequest $request, ProcedureType $procedureType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProcedureType $procedureType)
+    {
+        //
+    }
+}
