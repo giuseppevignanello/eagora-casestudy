23ff358fb1685245a0ff6068f35541a45c079144
giuseppevignanello <peppe.vignanello@gmail.com>
2023-12-08 18:37:15 +0100

    add main table

---
+++ resources/views/partials/main/main.blade.php
@@ -0,0 +1,45 @@
+<table class="main_table">
+    <thead class="table_head">
+        <tr>
+            <th>Titulo</th>
+            <th>Tipo Publicacion</th>
+            <th>Reservas</th>
+            <th>Estado</th>
+            <th>Creado</th>
+            <th>Caducidad</th>
+            <th>Acciones</th>
+        </tr>
+    </thead>
+    <tbody>
+
+        <tr>
+            <td>
+                <div class="d-flex align-items-center gap-2 title">
+                    <input type="checkbox">
+                    <span>Contenuto 1</span>
+                </div>
+            </td>
+            <td>Tipo 1</td>
+            <td>10</td>
+            <td>
+                <p class="status">
+                    <i class="fa-solid fa-circle-dot"></i> Activa
+                </p>
+            </td>
+            <td>2023-01-01</td>
+            <td>2023-12-31</td>
+            <td>
+                <div class="d-flex justify-content-center gap-3">
+                    <div>
+                        <i class="fa-solid fa-ellipsis"></i>
+                    </div>
+                    <div>
+                        <i class="fa-solid fa-trash"></i>
+                    </div>
+                </div>
+            </td>
+        </tr>
+
+
+    </tbody>
+</table>
--- resources/views/procedures/index.blade.php
+++ resources/views/procedures/index.blade.php
@@ -2,4 +2,5 @@
 
 @section('content')
     @include('partials.header.header')
+    @include('partials.main.main')
 @endsection
